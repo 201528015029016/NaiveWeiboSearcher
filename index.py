1ac2d0735e2dee10f0802ca1d37e44f63c898101
ChenYang@downtown-PC <cyvv888@hotmail.com>
2013-12-24 23:55:11 +0800

    almost complete

--- index.py
+++ index.py
@@ -6,85 +6,101 @@ import csv
 import pymongo
 import jieba
 import re
+import math
 
-class Indexer:
+cutPattern = u'@[-_0-9a-zA-Z\u4e00-\u9fa5]+[:\s]|@[-_0-9a-zA-Z\u4e00-\u9fa5]+$|\[.+\]|http://sinaurl\.cn/[0-9a-zA-Z]{5}|http://t\.cn/[0-9a-zA-Z]{7}'
+datafilter = re.compile(cutPattern)
+stopPattern = u'^`|~|!|@|#|\$|%|\^|&|\*|\(|\)|_|\+|-|=|\{|\}|\[|\]|\||\\|:|;|<|,|>|\.|\?|/|\s|\'|"|[A-Za-z]|[0-9]+$'
+stopfilter = re.compile(stopPattern)
+topicPattern = u'#[^#]+#'
+topicFilter = re.compile(topicPattern)
+with open('./library/stopList.csv','r') as stopListFile:
+	stopList = set()
+	reader = csv.reader(stopListFile)
+	for row in reader:
+		stopList.add(unicode(row[0],'utf-8'))	
+database = pymongo.database.Database(pymongo.MongoClient('localhost',27017), u'InvertedIndexDatabase')
+indexDB = database.indexDB
+articleDB = database.articleDB
+topicIndexDB = database.topicIndexDB
+topicDB = database.topicDB
 
-	def __init__(this):
-		this.cutPattern = u'@[-_0-9a-zA-Z\u4e00-\u9fa5]+[:\s]|@[-_0-9a-zA-Z\u4e00-\u9fa5]+$|\[.+\]|#.+#|http://sinaurl\.cn/[0-9a-zA-Z]{5}|http://t\.cn/[0-9a-zA-Z]{7}'
-		this.datafilter = re.compile(this.cutPattern)
-		
-		this.stopPattern = u'^`|~|!|@|#|\$|%|\^|&|\*|\(|\)|_|\+|-|=|\{|\}|\[|\]|\||\\|:|;|<|,|>|\.|\?|/|\s|\'|"|[A-Za-z]|[0-9]+$'
-		this.stopfilter = re.compile(this.stopPattern)
-		
-		with open('./library/stopList.csv','r') as stopListFile:
-			this.stopList = set()
-			reader = csv.reader(stopListFile)
-			for row in reader:
-				this.stopList.add(unicode(row[0],'utf-8'))
-			
-		this.invindex = {}
-		this.database = pymongo.database.Database(pymongo.MongoClient('localhost',27017), u'InvertedIndexDatabase')
-		this.indexDB = this.database.indexDB
-		this.articleDB = this.database.articleDB
-
-	'''
-	build invindex from input csv file at 'path'
-	input: path to csvfile
-	output: no output
-	'''
-	def Index(this, path):
-		docID = 0
-		with open(path,'r') as csvfile:
-			reader = csv.reader(csvfile,delimiter='\t')
-			for row in reader:		
-				docDict = {}
-				segtext = this.datafilter.split(unicode(row[2], 'utf-8'))
-				seglist = []
-				for item in segtext:
-					seglist.extend(jieba.cut(item))
-				for item in seglist:
-					if item not in this.stopList and not re.match(this.stopPattern,item):
-						tf = docDict.setdefault(item, 0)
-						docDict[item] = tf + 1
-				for item in docDict.iteritems():
-					table = this.invindex.setdefault(item[0],{})
-					table.setdefault(docID,item[1])	
-				
-				postArticle = {
-					u'DocID': docID, 
-					u'User': unicode(row[0], 'utf-8'), 
-					u'Time': unicode(row[1], 'utf-8'),#time.strptime(row[1], '%Y-%m-%d %H:%M'),
-					u'Article': unicode(row[2], 'utf-8')}
-				this.articleDB.save(postArticle)				
-				docID = docID + 1
-						
-		for index in this.invindex.iteritems():
-			df = len(index[1])
-			table = []
-			for item in index[1].iteritems():
-				table.append(tuple(item))
-			table.sort(cmp=lambda x,y: cmp(x[0],y[0]))
-			postIndex = {
-				u'Word': index[0],
-				u'DF': df,
-				u'IndexTable': table}
-			this.indexDB.save(postIndex)
 '''
-	def PrintIndexToFile(this):
-		with open('./indexAll.log','w') as output:
-			for index in this.invindex.iteritems():
-				output.write('%s:%d\n'%(index[0].encode('utf-8'), len(index[1])))
-				for item in index[1].iteritems():
-					output.write('[%d,%d]\t'%(item[0],item[1]))
-				output.write('\n\n')
+build invindex from input csv file at 'path'
+input: path to csvfile
+output: no output
 '''
-indexer = Indexer()
+def index(path):
+	docID = 0
+	invindex = {}
+	topicSet = set()
+	with open(path,'r') as csvfile:
+		reader = csv.reader(csvfile,delimiter='\t')
+		for row in reader:
+			docDict = {}
+			segtext = datafilter.split(unicode(row[2], 'utf-8'))
+			segTopic = topicFilter.findall(unicode(row[2], 'utf-8'))
+			seglist = []
+			for item in segtext:
+				seglist.extend(jieba.cut_for_search(item))
+			for item in seglist:
+				if item not in stopList and not re.match(stopPattern,item):
+					tf = docDict.setdefault(item, 0)
+					docDict[item] = tf + 1
+			for item in docDict.iteritems():
+				table = invindex.setdefault(item[0],{})
+				table.setdefault(docID,item[1])
+			for item in segTopic:
+				topicSet.add(item)
+			postArticle = {
+				u'DocID': docID, 
+				u'User': unicode(row[0], 'utf-8'), 
+				u'Time': unicode(row[1], 'utf-8'),#time.strptime(row[1], '%Y-%m-%d %H:%M'),
+				u'Article': unicode(row[2], 'utf-8')
+				}
+			articleDB.save(postArticle)
+			docID = docID + 1
+	N = docID
+	for index in invindex.iteritems():
+		df = len(index[1])
+		idf = math.log10(float(N)/float(df))
+		table = []
+		for item in index[1].iteritems():#calc tf*idf/docID (docID represents time in reverse order)
+			rank = round((math.log10(float(item[1]+1))*idf)/(math.log10(float(docID+1))), 3)
+			table.append((item[0], rank))
+		table.sort(cmp=lambda x,y: cmp(x[1],y[1]))
+		postIndex = {
+			u'Word': index[0],
+			u'DF': df,
+			u'IndexTable': table
+			}
+		indexDB.save(postIndex)
+	topicID = 0
+	topicindex = {}
+	for topic in topicSet:
+		seglist = jieba.cut_for_search(topic)
+		postTopic = {
+			u'TopicID': topicID, 
+			u'Topic': topic
+			}
+		for item in seglist:
+			if item not in stopList and not re.match(stopPattern,item):
+				table = topicindex.setdefault(item, [])
+				table.append(topicID)
+		topicDB.save(postTopic)
+		topicID = topicID + 1
+	N = topicID
+	for index in topicindex.iteritems():#topic's tf===1
+		postIndex = {
+			u'Word': index[0],
+			u'IDF': round(math.log10(float(N)/float(len(index[1]))),3),
+			u'IndexTable': index[1]
+			}
+		topicIndexDB.save(postIndex)
 try:
-	indexer.Index('./library/weibodata.csv')
+	index('./library/weibodata.csv')
 except Exception,e:
 	print(e)
 	raise
 else:
 	print("index build successfully")
-#indexer.PrintIndexToFile()
-
--- searcher.py
+++ searcher.py
@@ -1,9 +1,11 @@
 #!/usr/env/python
+# -*- coding: gb2312 -*-
 
 import pymongo
 import jieba
 import re
 import time
+#import thread
 
 class Searcher:
 
@@ -11,6 +13,124 @@ class Searcher:
 		this.database = pymongo.database.Database(pymongo.MongoClient('localhost',27017), u'InvertedIndexDatabase')
 		this.indexDB = this.database.indexDB
 		this.articleDB = this.database.articleDB
+		this.topicIndexDB = this.database.topicIndexDB
+		this.topicDB = this.database.topicDB
+		this.articleList = []
+		this.topicList = []
+
+	'''
+	sortOrder == 0: related
+	sortOrder == 1: time
+	'''
+	
+	def SearchArticle(this, seglist, sortOrder):
+		listOfIndex = []
+		articleList = {}
 		
-	def search(this, searchString):
-		seglist = jieba.cut(searchString)
+		try:
+			for item in seglist:
+				result = this.indexDB.find({u'Word': item})
+				for post in result:
+					i = 0
+					for item in post[u'IndexTable']:
+						weight = articleList.setdefault(item[0], [0.0,0])
+						articleList[item[0]][0] = weight[0] + item[1]
+						articleList[item[0]][1] = weight[1] + 1
+						i = i+1
+			if sortOrder==0:
+				this.articleList = sorted(articleList.iteritems(), cmp=lambda x,y:cmp(y[1][1],x[1][1]) or cmp(y[1][0],x[1][0]) or cmp(y[0],x[0]))
+			else:
+				this.articleList = sorted(articleList.iteritems(), cmp=lambda x,y:cmp(y[0],x[0]) or cmp(y[1][1],x[1][1]) or cmp(y[1][0],x[1][0]))
+		except:
+			print('error')
+			raise
+
+	def SearchRelated(this, seglist):
+		listOfIndex = []
+		topicList = {}
+		
+		try:
+			for item in seglist:
+				result = this.topicIndexDB.find({u'Word': item})
+
+				for post in result:
+					idf = post[u'IDF']
+					for item in post[u'IndexTable']:
+						weight = topicList.setdefault(item, [0.0,0])
+						topicList[item][0] = weight[0] + idf
+						topicList[item][1] = weight[1] + 1
+
+			this.topicList = sorted(topicList.iteritems(), cmp=lambda x,y:cmp(y[1][1],x[1][1]) or cmp(y[1][0],x[1][0]))
+		except:
+			print('error')
+			raise
+		
+	def Search(this, searchString, sortOrder):
+		seglist = jieba.cut_for_search(searchString)
+		timeSummary = {}
+		articleList = []
+		topicList = []
+		this.SearchArticle(seglist, sortOrder)
+		this.SearchRelated(seglist)
+		print('search complete')
+		i = 1
+		for article in this.articleList:
+			for post in this.articleDB.find({u'DocID':article[0]}):
+				postTime = time.strptime((post[u'Time'].split(' '))[0], u'%Y-%m-%d')
+				timeSummary.setdefault(postTime, 0)
+				timeSummary[postTime] = timeSummary[postTime] + 1
+				articleList.append(post)
+			i = i+1
+			if i>100:
+				break
+				
+		for topic in this.topicList:
+			for post in this.topicDB.find({u'TopicID':topic[0]}):
+				topicList.append(post)
+		
+		finalResult = {
+			u'Article': articleList,
+			u'Topic': topicList,
+			u'Summary': sorted(timeSummary.iteritems(),cmp = lambda x,y:cmp(x[0],y[0]))
+			}
+		return finalResult
+		
+	def ReOrderLast(this, sortOrder):
+		if sortOrder==0:
+			this.articleList.sort(cmp=lambda x,y:cmp(y[1][1],x[1][1]) or cmp(y[1][0],x[1][0]) or cmp(y[0],x[0]))
+		else:
+			this.articleList.sort(cmp=lambda x,y:cmp(y[0],x[0]) or cmp(y[1][1],x[1][1]) or cmp(y[1][0],x[1][0]))
+		articleList = []
+		i = 1
+		for article in this.articleList:
+			for post in this.articleDB.find({u'DocID':article[0]}):
+				articleList.append(post)
+			i = i+1
+			if i>100:
+				break
+		return articleList
+
+searcher = Searcher()
+result = searcher.Search(u'我相信我是傻逼',0)
+print('search result analysis complete')
+with open('dsb.log','w') as output:
+	output.write('related: ')
+	for topic in result[u'Topic']:
+		output.write('%s\n'%(topic[u'Topic'].encode('utf-8')))
+	output.write('\n')
+	for article in result[u'Article']:
+		output.write('%s\n'%(article[u'User'].encode('utf-8')))
+		output.write('%s\n'%(article[u'Article'].encode('utf-8')))
+		output.write('%s\n\n'%(article[u'Time'].encode('utf-8')))
+		
+	for item in result[u'Summary']:
+		output.write('%s:\t'%(time.strftime('%Y-%m-%d',item[0])))
+		output.write('%d\n'%(item[1]))
+		
+	result2 = searcher.ReOrderLast(1)
+	print('reorder complete')
+	for article in result2:
+		output.write('%s\n'%(article[u'User'].encode('utf-8')))
+		output.write('%s\n'%(article[u'Article'].encode('utf-8')))
+		output.write('%s\n\n'%(article[u'Time'].encode('utf-8')))
+
